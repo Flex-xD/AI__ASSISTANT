0a72f014a0244d2d5f042de126c77d203a6b6fee
Flex-xD <bishtaman88102@gmail.com>
2025-04-27 21:47:57 +0530

    fixed the issue in the route.ts

--- backend/src/routes/route.ts
+++ backend/src/routes/route.ts
@@ -1,7 +1,7 @@
-import express from "express";
+import express, { RequestHandler } from "express";
 import { LLMprocessingForTwitter } from "../controller/LLMprocessingController";
 const router = express.Router();
 
-router.post("/processing" , LLMprocessingForTwitter)
+router.post("/processing" , LLMprocessingForTwitter as RequestHandler)
 
 export default router;
