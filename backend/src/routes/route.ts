ff23777a9d131ab6b74a1315a2bc7ef836cee16e
Flex-xD <bishtaman88102@gmail.com>
2025-05-11 17:39:10 +0530

    trying to add both llm processing and uploading the processed data in one controller , but I am getting and invalid error related to Twitter api !

--- backend/src/routes/route.ts
+++ backend/src/routes/route.ts
@@ -1,7 +1,9 @@
 import express, { RequestHandler } from "express";
 import { LLMprocessingForX } from "../controllers/processing/LLMprocessingController";
+import { TweetUploading } from "../controllers/uploading";
 const router = express.Router();
 
 router.post("/processing" , LLMprocessingForX as unknown as RequestHandler)
+router.post("/uploading/twitter", TweetUploading as unknown as RequestHandler)
 
 export default router;
