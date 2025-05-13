15ad66983662053a6079d32d1bac095d5c8d6ff4
Flex-xD <bishtaman88102@gmail.com>
2025-05-13 16:59:09 +0530

    got to know the problem , it was giving me 403 because of the merging of the AI reponse and tweeting it suddenly

--- backend/src/routes/route.ts
+++ backend/src/routes/route.ts
@@ -1,9 +1,8 @@
 import express, { RequestHandler } from "express";
-import { LLMprocessingForX } from "../controllers/processing/LLMprocessingController";
-import { TweetUploading } from "../controllers/uploading";
+import { LLMprocessingForX, tweetUploading } from "../controllers/uploading";
 const router = express.Router();
 
-router.post("/processing" , LLMprocessingForX as unknown as RequestHandler)
-router.post("/uploading/twitter", TweetUploading as unknown as RequestHandler)
+router.post("/uploading/twitter", tweetUploading as unknown as RequestHandler)
+router.post("/processing", LLMprocessingForX as unknown as RequestHandler)
 
 export default router;
