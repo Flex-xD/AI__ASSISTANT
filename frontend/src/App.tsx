ceec05eb067544c13e3f12850182d4aed68a1090
Flex-xD <bishtaman88102@gmail.com>
2025-05-14 17:10:06 +0530

    started working on the frontend of the project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+    return (
+      <div className='h-screen w-screen flex justify-center items-center bg-gray-300'>
+        <div className="flex items-center justify-center w-3/4 h-3/4 rounded-3xl shadow-2xl">
+        <div>
+          <input type="text" className=" border border-black h-12 w-3/4 rounded-full " />
+          <button>Submit</button>
+        </div>
+        </div>
+      </div>
+    )
+}
+
+export default App
